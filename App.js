2d0662dad03c97e6c22feec1f8503e4ff4c6d2b4
Anıl <anil@Anl-MacBook-Air.local>
2023-10-14 22:12:47 +0300

    basic display is finished

--- App.js
+++ App.js
@@ -1,23 +1,9 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
-import HomeScreen from './src/screens/Home';
-import Post from './src/components/Post';
-import feed from './assets/data/feed';
-import SearchResultsScreen from './src/screens/SearchResults';
-import DestinationSearchScreen from './src/screens/DestinationSearch';
-import GuestsScreen from './src/screens/Guests';
-const App = () => {
-  const post1 = feed[0];
-  const post2 = feed[1];
+import 'react-native-gesture-handler';
+import Router from './src/navigation/Router';
 
-  return (
-    <SafeAreaView>
-      {/*<HomeScreen />*/}
-      {/*   <Post post={post1} /> */}
-      {/*<SearchResultsScreen />*/}
-      {/*<DestinationSearchScreen />*/}
-      <GuestsScreen />
-    </SafeAreaView>
-  );
+const App = () => {
+  return <Router />;
 };
 export default App;
--- src/screens/DestinationSearch/index.js
+++ src/screens/DestinationSearch/index.js
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
 import styles from './styles';
-import {View, TextInput, FlatList, Text} from 'react-native';
+import {View, TextInput, FlatList, Text, Pressable} from 'react-native';
 import searchResult from '../../../assets/data/search';
 import Entypo from 'react-native-vector-icons/Entypo';
+import {useNavigation} from '@react-navigation/native';
 
 const DestinationSearchScreen = props => {
+  const navigation = useNavigation();
   const [inputText, setInputText] = useState('');
 
   return (
@@ -20,12 +22,16 @@ const DestinationSearchScreen = props => {
       <FlatList
         data={searchResult}
         renderItem={({item}) => (
-          <View style={styles.row}>
+          <Pressable
+            style={styles.row}
+            onPress={() => {
+              navigation.navigate('Guests');
+            }}>
             <View style={styles.iconContainer}>
               <Entypo name={'location-pin'} size={30} />
             </View>
             <Text style={styles.locationText}>{item.description}</Text>
-          </View>
+          </Pressable>
         )}
       />
     </View>
